08f12f53e20950eec94b0643366b7582fa8297ee
julianagraham <julzlee6@outlook.com>
2026-09-12 10:41:01 -0400

    Add player name and starting inventory

--- HauntedEscape/HauntedEscape.java
+++ HauntedEscape/HauntedEscape.java
@@ -11,6 +11,20 @@ public class HauntedEscape {
         System.out.println("===============================");
         System.out.println();
         System.out.println("Escape the mansion before it's too late!");
+        System.out.println();
+        System.out.println("What is your name? ");
+        String playerName = scanner.nextLine();
+        System.out.println();
+        int fear = 0;
+        int keys = 0;
+        int candles = 1;
+        System.out.println("Welcome to Blackwood Mansion, " +playerName + "!");
+        System.out.println();
+        System.out.println("STARTING INVENTORY");
+        System.out.println();
+        System.out.println("Keys: " + keys);
+        System.out.println("Candles: " + candles);
+        System.out.println("Fear: " + fear + "/100");
 
         scanner.close();
     }
