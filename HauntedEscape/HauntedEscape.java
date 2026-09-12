4de1fc2e57f75255b73a2e2d627c20f42bdca57d
julianagraham <julzlee6@outlook.com>
2026-09-12 10:34:21 -0400

    Create Haunted Escape project

---
+++ HauntedEscape/HauntedEscape.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HauntedEscape {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("===============================");
+        System.out.println("        HAUNTED ESCAPE         ");
+        System.out.println("===============================");
+        System.out.println();
+        System.out.println("Escape the mansion before it's too late!");
+
+        scanner.close();
+    }
+}
