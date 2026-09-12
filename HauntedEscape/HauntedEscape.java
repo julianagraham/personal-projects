71074cfae3cf82d7ca119830035244eb87529792
julianagraham <julzlee6@outlook.com>
2026-09-12 10:55:55 -0400

    Added room branches

--- HauntedEscape/HauntedEscape.java
+++ HauntedEscape/HauntedEscape.java
@@ -40,13 +40,34 @@ public class HauntedEscape {
 
         if (roomChoice == 1) {
             System.out.println("You enter the kitchen.");
+            System.out.println("You find a rusty key beside the sink.");
+
+            keys += 1;
+            fear += 5;
+
         } else if (roomChoice == 2) {
             System.out.println("You go upstairs.");
+            System.out.println("You find an old candle on a dresser.");
+
+            candles += 1;
+            fear += 15;
+
         } else if (roomChoice == 3) {
             System.out.println("You enter the basement.");
+            System.out.println("Something whispers you name from the darkness.");
+
+            fear += 30;
+
         } else {
             System.out.println("Invalid choice.");
         }
+
+        System.out.println();
+        System.out.println("CURRENT STATUS");
+        System.out.println();
+        System.out.println("Keys: " + keys);
+        System.out.println("Candles: " + candles);
+        System.out.println("Fear: " + fear + "/100");
 
         scanner.close();
     }
