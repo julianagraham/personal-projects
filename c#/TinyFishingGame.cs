97de0c1dda68a3d58e109affe8ca4a59704bef5e
julianagraham <julzlee6@outlook.com>
2026-09-17 21:48:25 -0400

    Bait purchasing logic

--- c#/TinyFishingGame.cs
+++ c#/TinyFishingGame.cs
@@ -4,14 +4,15 @@ namespace FishingPractice
     {
         static void Main(string[] args)
         {
-            // adding variables
+            // Player's starting variables
             string playerName = "Jules";
             int coins = 20;
             int fishCaught = 0;
             int rodLevel = 1;
-            bool hasBait = true;
+            bool hasBait = false;
+            int baitPrice = 5;
 
-            // adding starting stats
+            // Display the player's starting stats
             Console.WriteLine("UNDERWATER FISHING");
             Console.WriteLine();
             Console.WriteLine($"Welcome, {playerName}!");
@@ -21,6 +22,28 @@ namespace FishingPractice
             Console.WriteLine($"Fish caught: {fishCaught}");
             Console.WriteLine($"Rod Level: {rodLevel}");
             Console.WriteLine($"Bait Available: {hasBait}");
+
+            // Check whether the player already has bait
+            if (hasBait)
+            {
+                Console.WriteLine("Your fishing rod is ready!");
+            } else
+            {
+                Console.WriteLine("You need more bait!");
+            }
+
+            // Check whether the player can afford bait
+            if (coins >= baitPrice)
+            {
+                Console.WriteLine("You bought some bait!");
+                coins -= baitPrice;
+                hasBait = true;
+                Console.WriteLine($"Coins remaining: {coins}");
+                Console.WriteLine($"Bait available: {hasBait}");
+            } else
+            {
+                Console.WriteLine("You don't have enough coins to buy bait!");
+            }
         }
     }
 }
